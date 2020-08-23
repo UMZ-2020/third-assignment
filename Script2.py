4f3f9d1c7dd893c8c388785db511fcc5bbd02b1c
Kamil Czernuszewicz <kamil@czernuszewicz.pl>
2020-08-23 20:24:34 +0200

    Task #4

--- Script2.py
+++ Script2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
 df_results = pd.read_csv('survey_results_public.csv',
@@ -31,16 +32,16 @@ IQR = Q3 - Q1
 
 df_results = df_results[~((X_train < (Q1 - 1.5 * IQR)) | (X_train > (Q3 + 1.5 * IQR))).any(axis=1)]
 
-X_train = df_results[['Age', 'Hobbyist', 'YearsCode', 'Dependents']]
-y_train = df_results.Student
+X_train, X_test, y_train, y_test = train_test_split(
+    df_results[['Age', 'Hobbyist', 'YearsCode', 'Dependents']], df_results.Student, test_size=0.2, random_state=777)
 
 log_reg = LogisticRegression()
 log_reg.fit(X_train, y_train)
 
-y_train_pred = log_reg.predict(X_train)
+y_test_pred = log_reg.predict(X_test)
 
-zer_rule_accuracy = 1 - (sum(y_train) / len(y_train))
-train_accuracy = sum(y_train == y_train_pred) / len(y_train_pred)
-train_sensitivity = sum(y_train & y_train_pred) / sum(y_train_pred)
-train_specificity = sum(-~-(y_train | y_train_pred)) / (len(y_train_pred) - sum(y_train_pred))
-f_train = f1_score(y_train, y_train_pred)
+zer_rule_accuracy = 1 - (sum(y_test) / len(y_test))
+train_accuracy = sum(y_test == y_test_pred) / len(y_test_pred)
+train_sensitivity = sum(y_test & y_test_pred) / sum(y_test_pred)
+train_specificity = sum(-~-(y_test | y_test_pred)) / (len(y_test_pred) - sum(y_test_pred))
+f_train = f1_score(y_test, y_test_pred)
