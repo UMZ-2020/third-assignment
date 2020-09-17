2d93b544f9e4d21fe7d08ee77607890d99d54968
Paulina Pielacinska <pielacinskapaulina@gmail.com>
2020-09-17 23:33:17 +0200

    Completed script 1

--- script1.py
+++ script1.py
@@ -5,16 +5,12 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import fbeta_score
-import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay
-
 
 import seaborn as sns
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 
@@ -29,7 +25,7 @@ df_names = ['Occupancy', 'Date', 'Temperature', 'Humidity',
 df = pd.read_csv(data_file, sep='\t', names=df_names)
 df = df.dropna()
 
-# logistic regression classifier on one independent variable - Date
+# logistic regression classifier on one independent variable - Light
 clf = LogisticRegression()
 X_train = df[['Light']]
 y_train = df.Occupancy
@@ -48,6 +44,13 @@ conf_matrix = confusion_matrix(y_train, y_train_pred)
 tn, fp, fn, tp = conf_matrix.ravel()
 tn, fp, fn, tp
 specificity = tn/(tn+fp)
+
+#measure f for train data
+f1_score(y_train, y_train_pred)
+# In the room should be a person
+fbeta_score(y_train, y_train_pred, beta=0.5)
+# In the room shouldn't be a person
+fbeta_score(y_train, y_train_pred, beta=2)
 
 # logistic regression classifier on all but date independent variables
 clf_all = LogisticRegression()
@@ -68,16 +71,24 @@ tn, fp, fn, tp = conf_matrix.ravel()
 tn, fp, fn, tp 
 specificity = tn/(tn+fp)
 
+#measure f for train data
+f1_score(y_train, y_train_pred_all)
+# In the room should be a person
+fbeta_score(y_train, y_train_pred_all, beta=0.5)
+# In the room shouldn't be a person
+fbeta_score(y_train, y_train_pred_all, beta=2)
+
+
 df_column_names = ['Date', 'Temperature', 'Humidity', 'Light',
                    'CO2', 'HumidityRatio']
 X_column_names = ['Temperature', 'Humidity', 'Light', 'CO2', 'HumidityRatio']
 
-X_test = pd.read_csv(test_data_file, sep='\t', names=df_column_names, usecols=X_column_names)
+X_test_all = pd.read_csv(test_data_file, sep='\t', names=df_column_names, usecols=X_column_names)
 df_results = pd.read_csv(results_file, sep='\t', names=['y'])
 df_results['y'] = df_results['y'].astype('category')
 
 y_true = df_results['y']
-y_test_pred = clf_all.predict(X_test)
+y_test_pred = clf_all.predict(X_test_all)
 #calculate accuracy
 clf_test_accuracy = accuracy_score(y_true, y_test_pred)
 #calculate sensitivity
@@ -88,4 +99,14 @@ tn, fp, fn, tp = conf_matrix.ravel()
 tn, fp, fn, tp 
 specificity = tn/(tn+fp)
 
-ConfusionMatrixDisplay(y_true, y_test_pred, df_results['y'])
+#measure f for test data
+f1_score(y_true, y_test_pred)
+# In the room should be a person
+fbeta_score(y_true, y_test_pred, beta=0.5)
+# In the room shouldn't be a person
+fbeta_score(y_true, y_test_pred, beta=2)
+
+
+pd.DataFrame(y_test_pred).to_csv(output_file, index=False, header=False)
+
+
